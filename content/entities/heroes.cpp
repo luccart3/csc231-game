e998d346e6d6d2b3fdef04fd9336b13206e0075f
luccart <luc.cart03@gmail.com>
2024-04-10 13:34:55 -0500

    Added to heroes.cpp/.h

--- content/entities/heroes.cpp
+++ content/entities/heroes.cpp
@@ -1,5 +1,8 @@
 #include "heroes.h"
 
 namespace Heroes {
-
+    void make_ogre(std::shared_ptr<Entity>& hero){
+        hero->set_sprite("ogre");
+        hero->set_max_health((50));
+    }
 }
--- content/entities/heroes.h
+++ content/entities/heroes.h
@@ -1,5 +1,10 @@
 #pragma once
+#include <memory>
+#include "entity.h"
+#include "action.h"
+
 
 namespace Heroes {
-
+    void make_ogre(std::shared_ptr<Entity>& hero);
+    std::unique_ptr<Action> behavior();
 }
