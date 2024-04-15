30f690ee3b3c2a3d9a05ecc95bb469d118dcfda7
luccart <luc.cart03@gmail.com>
2024-04-15 11:50:18 -0500

    Finished Hero.cpp/.h

--- content/entities/heroes.cpp
+++ content/entities/heroes.cpp
@@ -1,8 +1,33 @@
 #include "heroes.h"
+#include "action.h"
+#include "engine.h"
+#include "rest.h"
+#include "move.h"
 
 namespace Heroes {
-    void make_ogre(std::shared_ptr<Entity>& hero){
-        hero->set_sprite("ogre");
-        hero->set_max_health((50));
+void make_ogre(std::shared_ptr<Entity>& hero) {
+    hero->set_sprite("ogre");
+    hero->set_max_health((50));
+    hero->behavior = behavior;
+}
+std::unique_ptr<Action> behavior(Engine& engine, Entity&) {
+    std::string key = engine.input.get_last_keypress();
+    if (key == "R") {
+        return std::make_unique<Rest>();
     }
+    if (key == "D") {
+        return std::make_unique<Move>(Vec{1, 0});
+    }
+    if (key == "A") {
+        return std::make_unique<Move>(Vec{-1, 0});
+    }
+    if (key == "W") {
+        return std::make_unique<Move>(Vec{0, 1});
+    }
+    if (key == "S") {
+        return std::make_unique<Move>(Vec{0, -1});
+    }
+    return nullptr;
+}
+
 }
--- content/entities/heroes.h
+++ content/entities/heroes.h
@@ -1,10 +1,11 @@
 #pragma once
-#include <memory>
-#include "entity.h"
 #include "action.h"
+#include "vec.h"
+#include "entity.h"
+#include "engine.h"
 
 
 namespace Heroes {
     void make_ogre(std::shared_ptr<Entity>& hero);
-    std::unique_ptr<Action> behavior();
+    std::unique_ptr<Action> behavior(Engine& engine, Entity&);
 }
