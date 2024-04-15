4cb5141bb47118d18c76d6250e6385ae21313828
luccart <luc.cart03@gmail.com>
2024-04-15 11:50:29 -0500

    Working on move.cpp

--- content/actions/move.cpp
+++ content/actions/move.cpp
@@ -1,19 +1,39 @@
 #include "move.h"
-#include "tile.h"
-#include "engine.h"
+#include "entity.h"
+#include <iostream>
 
-Result Move::perform(Engine&, std::shared_ptr<Entity>) {
+Result Move::perform(Engine& engine, std::shared_ptr<Entity> entity) {
+    Move m = Move(direction);
+    Vec position = entity->get_position();
+    Tile& tile = engine.dungeon.get_tile(position);
+
+    if (tile.is_wall() || tile.has_entity()) {
+        return failure();
+    }
+
+    else if (tile.has_door()) {
+        return failure();
+    }
+
+    else {
+        entity->move_to(position);
+        return success();
+    }
+}
+
+Move::Move(Vec direction) {
+    Vec position;
     Vec new_position = position + direction;
-    position = new_position;
-    //Tile& tile = engine.dungeon.get_tile(position);
+}
+
+
 //if (tile = door) { go through door }
 
-//else if (tile = enemy ) { return failure () }
+//else if (tile = entity ) { return failure () }
 
 //else if (tile = wall ) { return failure () }
 
 //else { new_position = old_position + direction}
-}
 
 // If new position is wall
 // return failure()
--- content/actions/move.h
+++ content/actions/move.h
@@ -1,10 +1,16 @@
 #include "action.h"
 #include "vec.h"
+#include "engine.h"
+#include "tile.h"
+#include "entity.h"
 
 
 class Move : public Action {
-    Result perform(Engine& engine, std::shared_ptr<Entity> entity) override;
-    Vec position;
-    Vec direction;
+public:
+    explicit Move(Vec direction);
 
+    Result perform(Engine& engine, std::shared_ptr<Entity> entity) override;
+
+private:
+    Vec direction;
 };
