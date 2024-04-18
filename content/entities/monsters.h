ba57e0fd5087e5347a607661898dac13904cce6b
luccart <luc.cart03@gmail.com>
2024-04-17 23:53:51 -0500

    Created 3 Monsters and 3 Behaviors in monsters.cpp/.h

--- content/entities/monsters.h
+++ content/entities/monsters.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "action.h"
+#include "vec.h"
+#include "entity.h"
+#include "engine.h"
+
 
 namespace Monsters {
-    
+    void make_orc_masked(std::shared_ptr<Entity>& monster);
+    void make_muddy(std::shared_ptr<Entity>& monster);
+    void make_demon_big(std::shared_ptr<Entity>& monster);
+
+    std::unique_ptr<Action> behavior_aggressive(Engine& engine, Entity&);
+    std::unique_ptr<Action> behavior_wander(Engine& engine, Entity&);
+    std::unique_ptr<Action> behavior_boring(Engine& engine, Entity&);
+
 }
