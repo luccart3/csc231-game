c3654d7eeebbf067e8994b802456c3287fdd68b9
luccart <luc.cart03@gmail.com>
2024-04-17 23:53:27 -0500

    added monsters to main

--- main.cpp
+++ main.cpp
@@ -10,6 +10,17 @@ int main() {
 
         std::shared_ptr<Entity> hero = engine.create_hero();
         Heroes::make_ogre(hero);
+
+        for (int i = 0; i < 5; ++i) {
+            std::shared_ptr<Entity> monster_1 = engine.create_monster();
+            std::shared_ptr<Entity> monster_2 = engine.create_monster();
+            std::shared_ptr<Entity> monster_3 = engine.create_monster();
+
+            Monsters::make_orc_masked(monster_1);
+            Monsters::make_muddy(monster_2);
+            Monsters::make_demon_big(monster_3);
+        }
+
         engine.run();
     }
     catch (std::exception& e) {
