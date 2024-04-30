206cb39adbb1ff5cf594c825e80a46bc640f664e
luccart <luc.cart03@gmail.com>
2024-04-30 18:25:10 -0500

    added drink to healing_potion.cpp

--- content/items/healing_potion.cpp
+++ content/items/healing_potion.cpp
@@ -2,13 +2,15 @@
 #include "engine.h"
 #include "entity.h"
 #include "recover.h"
+#include "drink.h"
+#include "sound.h"
 
-// content/items/spear.cpp
 Healing_Potion::Healing_Potion(int recovery)
     :Item{"potion_red"}, recovery{recovery} {}
 
-void Healing_Potion::use(Engine& engine, Entity& attacker, Entity& defender) {
-//    engine.events.create_event<Thrust>(sprite, attacker.get_direction());
+void Healing_Potion::use(Engine& engine, Entity& attacker, Entity&) {
+    engine.events.create_event<Drink>(sprite, attacker.get_direction());
     engine.events.create_event<Recover>(attacker, recovery);
+    engine.events.create_event<Sound>("slurp");
 }
 
