3c95d590d2188087eb22144073b0fa9675cac9f3
luccart <luc.cart03@gmail.com>
2024-04-29 21:47:25 -0500

    gave spiked_club.cpp sound and animation

--- content/items/spiked_club.cpp
+++ content/items/spiked_club.cpp
@@ -1,11 +1,25 @@
 #include "spiked_club.h"
 #include "hit.h"
 #include "engine.h"
+#include "swing.h"
+#include "entity.h"
+#include "animation.h"
+#include "sound.h"
 
 Spiked_Club::Spiked_Club(int damage)
     :Item{"spiked_club"}, damage{damage} {}
 
-void Spiked_Club::use(Engine& engine, Entity&, Entity& defender) {
-    engine.events.create_event<Hit>(defender, damage);
+void Spiked_Club::use(Engine& engine, Entity& attacker, Entity& defender) {
+    Vec direction = defender.get_position() - attacker.get_position();
+
+    auto swing = engine.events.create_event<Swing>(sprite, direction);
+    std::shared_ptr<Event> explosion =
+        std::make_shared<Animation>("explosion", defender.get_position());
+    explosion->add_next(Hit{defender, damage});
+    swing->add_next(explosion);
+    engine.events.create_event<Sound>("thunder");
+
 }
+
+
 
