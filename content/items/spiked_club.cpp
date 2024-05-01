aa104d506c84b0b83975b5b4ae9c7e3e2ad066f8
luccart <luc.cart03@gmail.com>
2024-04-30 19:57:36 -0500

    removed explosion from spiked_club.cpp

--- content/items/spiked_club.cpp
+++ content/items/spiked_club.cpp
@@ -3,21 +3,15 @@
 #include "engine.h"
 #include "swing.h"
 #include "entity.h"
-#include "animation.h"
 #include "sound.h"
 
 Spiked_Club::Spiked_Club(int damage)
     :Item{"spiked_club"}, damage{damage} {}
 
 void Spiked_Club::use(Engine& engine, Entity& attacker, Entity& defender) {
-    Vec direction = defender.get_position() - attacker.get_position();
-
-    auto swing = engine.events.create_event<Swing>(sprite, direction);
-    std::shared_ptr<Event> explosion =
-        std::make_shared<Animation>("explosion", defender.get_position());
-    explosion->add_next(Hit{defender, damage});
-    swing->add_next(explosion);
-    engine.events.create_event<Sound>("thunder");
+    engine.events.create_event<Swing>(sprite, attacker.get_direction());
+    engine.events.create_event<Sound>("hit");
+    engine.events.create_event<Hit>(defender, damage);
 
 }
 
