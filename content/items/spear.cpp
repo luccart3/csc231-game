7e5df05e70191f33e4feb1551914d219b84883cc
luccart <luc.cart03@gmail.com>
2024-04-30 19:17:13 -0500

    spear and sword now make hit sound

--- content/items/spear.cpp
+++ content/items/spear.cpp
@@ -3,6 +3,7 @@
 #include "engine.h"
 #include "thrust.h"
 #include "entity.h"
+#include "sound.h"
 
 // content/items/spear.cpp
 Spear::Spear(int damage)
@@ -10,6 +11,7 @@ Spear::Spear(int damage)
 
 void Spear::use(Engine& engine, Entity& attacker, Entity& defender) {
     engine.events.create_event<Thrust>(sprite, attacker.get_direction());
+    engine.events.create_event<Sound>("hit");
     engine.events.create_event<Hit>(defender, damage);
 }
 
--- content/items/sword_rusty.cpp
+++ content/items/sword_rusty.cpp
@@ -3,6 +3,7 @@
 #include "engine.h"
 #include "swing.h"
 #include "entity.h"
+#include "sound.h"
 
 // content/items/spear.cpp
 Sword_Rusty::Sword_Rusty(int damage)
@@ -10,6 +11,7 @@ Sword_Rusty::Sword_Rusty(int damage)
 
 void Sword_Rusty::use(Engine& engine, Entity& attacker, Entity& defender) {
     engine.events.create_event<Swing>(sprite, attacker.get_direction());
+    engine.events.create_event<Sound>("hit");
     engine.events.create_event<Hit>(defender, damage);
 }
 
