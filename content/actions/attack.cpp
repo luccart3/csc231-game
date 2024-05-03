43575e2c56634d0aa9716baa43c5346c1ed1db4c
luccart <luc.cart03@gmail.com>
2024-05-02 20:12:41 -0500

    updated

--- content/actions/attack.cpp
+++ content/actions/attack.cpp
@@ -8,5 +8,5 @@ Attack::Attack(Entity& defender)
 Result Attack::perform(Engine& engine, std::shared_ptr<Entity> attacker) {
     std::shared_ptr<Item> weapon = attacker->get_current_item();
     weapon->use(engine, *attacker, defender);
-     return success();
+    return success();
 }
