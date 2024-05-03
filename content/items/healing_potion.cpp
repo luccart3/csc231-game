5eaffd9087feb2295e27e5968c85c5cd7bd4f276
luccart <luc.cart03@gmail.com>
2024-05-02 20:13:00 -0500

    added sequencing to healing_potion.cpp

--- content/items/healing_potion.cpp
+++ content/items/healing_potion.cpp
@@ -4,13 +4,20 @@
 #include "recover.h"
 #include "drink.h"
 #include "sound.h"
+#include "remove_item.h"
 
 Healing_Potion::Healing_Potion(int recovery)
     :Item{"potion_red"}, recovery{recovery} {}
 
 void Healing_Potion::use(Engine& engine, Entity& owner) {
-    engine.events.create_event<Drink>(sprite, owner.get_direction());
-    engine.events.create_event<Recover>(owner, recovery);
+
+    Vec direction = owner.get_direction();
+
+    auto drink = engine.events.create_event<Drink>(sprite, direction);
+    std::shared_ptr<Event> remove_item =
+        std::make_shared<RemoveItem>(owner, *this);
+    drink->add_next(Recover{owner, recovery});
+    drink->add_next(remove_item);
     engine.events.create_event<Sound>("slurp");
 }
 
