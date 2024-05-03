939ba2c4f6e230508c55ee6a52abdc7843a2d6bd
luccart <luc.cart03@gmail.com>
2024-05-02 22:22:21 -0500

    created Interact action for items in dungeon

--- content/actions/interact.cpp
+++ content/actions/interact.cpp
@@ -1,9 +1,10 @@
 #include "interact.h"
 #include "item.h"
 
+Interact::Interact(Tile& tile)
+    : tile{tile}{}
+
 Result Interact::perform(Engine& engine, std::shared_ptr<Entity> entity) {
-    Vec pos = entity->get_position();
-    Tile& tile = engine.dungeon.get_tile(pos);
     tile.item->interact(engine, *entity);
     return success();
 }
--- content/actions/interact.h
+++ content/actions/interact.h
@@ -2,8 +2,13 @@
 #include "entity.h"
 #include "engine.h"
 #include "action.h"
+#include "tile.h"
 
 class Interact : public Action {
 public:
+    explicit Interact(Tile& tile);
+
     Result perform(Engine& engine, std::shared_ptr<Entity> entity) override;
+private:
+    Tile& tile;
 };
